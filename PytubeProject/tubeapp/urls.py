c2973c632ec2cf689c1f9fd8edca49bcc101abe3
FelipeTDR31 <felipetdrgames@gmail.com>
2024-07-18 08:41:27 -0300

    fixed the downloads for the docker container

--- PytubeProject/tubeapp/urls.py
+++ PytubeProject/tubeapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from tubeapp.views import *
+from tubeapp.views import index, search, prepare, download
 
 urlpatterns = [
     path('', index),
     path('/search', search),
-    path('/download', download),
+    path('/prepare', prepare),
+    path('/download/<str:name>', download)
 ]
--- PytubeProject/tubeapp/views.py
+++ PytubeProject/tubeapp/views.py
@@ -6,16 +6,23 @@ from pytubefix import YouTube
 import random
 import shutil
 import os
+from django.http import FileResponse
 # Create your views here.
 
 def merge_video_audio(video_path, audio_path, output_path):
     # Construct the FFmpeg command
-    command = f'ffmpeg -i {video_path} -i {audio_path} -c:v copy -c:a aac {output_path}'
+    command = f'ffmpeg  -y -i {video_path} -i {audio_path} -c:v copy -c:a aac {output_path}'
 
     # Run the FFmpeg command as a subprocess
     subprocess.run(command, shell=True)
     folder_path = os.path.dirname(video_path)
     shutil.rmtree(folder_path)
+
+def download(request, name):
+    if request.method == 'GET':
+        output_path = "videos/"+name+".mp4"
+        file = open(output_path, 'rb')
+        return FileResponse(file, as_attachment=True, filename=name+".mp4", content_type='video/mp4')
 
 def index(request):
     return render(request, 'index.html')
@@ -42,7 +49,7 @@ def search(request):
         title = yt.title
         return JsonResponse({'title': title, 'streams': streamsJson, 'audioStreams': audioStreamsJson})
 
-def download (request):
+def prepare (request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         url = data['url']
@@ -52,11 +59,11 @@ def download (request):
         videoStream = yt.streams.get_by_itag(selectedStream)
         audioStream = yt.streams.get_by_itag(selectedAudioStream)
         randomID = random.randint(1,1000)
-        videoStream.download(output_path="C:/Users/Prefeitura/Downloads/"+str(randomID), filename="video"+".mp4")
-        audioStream.download(output_path="C:/Users/Prefeitura/Downloads/"+str(randomID), filename="audio"+".mp4")
-        videoPath = "C:/Users/Prefeitura/Downloads/"+str(randomID)+"/"+"video"+".mp4"
-        audioPath = "C:/Users/Prefeitura/Downloads/"+str(randomID)+"/"+"audio"+".mp4"
-        output_path = "C:/Users/Prefeitura/Downloads/"+''.join(e for e in yt.title if e.isalnum() or e == '_')+".mp4"
+        videoStream.download(output_path=str(randomID), filename="video"+".mp4")
+        audioStream.download(output_path=str(randomID), filename="audio"+".mp4")
+        videoPath = str(randomID)+"/"+"video"+".mp4"
+        audioPath = str(randomID)+"/"+"audio"+".mp4"
+        output_path = "videos/"+''.join(e for e in yt.title if e.isalnum() or e == '_')+".mp4"
+        file_name = ''.join(e for e in yt.title if e.isalnum() or e == '_')
         merge_video_audio(videoPath, audioPath, output_path)
-
-        return JsonResponse({'status': 'success'})
+        return JsonResponse({'file_name': file_name})
