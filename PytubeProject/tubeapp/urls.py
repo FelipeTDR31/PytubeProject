49ecaaa2e7b1f58dcfde80bdd1ef56738e8504d4
FelipeTDR31 <felipetdrgames@gmail.com>
2024-07-18 13:26:46 -0300

    added search button

--- PytubeProject/tubeapp/urls.py
+++ PytubeProject/tubeapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from tubeapp.views import index, search, prepare, download
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='tubeapp'),
     path('search', search),
     path('prepare', prepare),
     path('download/<str:name>', download)
